17c63d8b2bccc33c2a1dc90a5431d3a7e81bd5dd
Martin Smith <martinsmith1968@gmail.com>
2025-01-09 17:44:22 +0000

    Supporting calculation classes

---
+++ Stopwatch/TimerDisplayBuilder.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "stdafx.h"
+#include "Timer.h"
+#include <string>
+#include <iomanip>
+
+// ReSharper disable CppInconsistentNaming
+// ReSharper disable CppClangTidyCppcoreguidelinesAvoidConstOrRefDataMembers
+// ReSharper disable CppClangTidyClangDiagnosticHeaderHygiene
+
+using namespace std;
+
+namespace Stopwatch
+{
+    class TimerDisplayBuilder
+    {
+    public:
+        static const string DefaultElapsedTimeTextFormat;
+        static const string DefaultStartTimeTextFormat;
+
+        [[nodiscard]] static string GetFormattedText(const Timer& timer, const string& format);
+        [[nodiscard]] static string GetFormattedElapsedTime(double timespan, const string& format);
+        [[nodiscard]] static string GetFormattedStartTime(const tm& tm, const string& format);
+    };
+}
