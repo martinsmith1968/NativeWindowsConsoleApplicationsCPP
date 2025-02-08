f665ace69fca2ca603bbd0ced3b98e9bce569b77
Martin Smith <martinsmith1968@gmail.com>
2025-02-08 19:56:18 +0000

    Task/getdatetime formatting (#15)
    * Start with a chrono clock
    
    * Calculate milliseconds portion of a system_clock point in time
    
    * Rely of strftime formats and supplement with those not supported
    
    * Replace non threadsafe localtime call

--- ShowDateTime/AppArguments.h
+++ ShowDateTime/AppArguments.h
@@ -9,9 +9,17 @@
 // ReSharper disable CppClangTidyClangDiagnosticHeaderHygiene
 // ReSharper disable CppTooWideScopeInitStatement
 // ReSharper disable CppClangTidyCppcoreguidelinesAvoidConstOrRefDataMembers
+// ReSharper disable CppDeclaratorNeverUsed
 
+using namespace std;
+using namespace std::chrono;
 using namespace DNX::Utils;
 
+// Sources:
+// https://ambreen2006.medium.com/exploring-date-and-time-with-chrono-3a8e9af60f62
+// https://stackoverflow.com/questions/10654258/get-millisecond-part-of-time
+// https://en.cppreference.com/w/cpp/chrono/system_clock
+// https://en.cppreference.com/w/cpp/chrono/time_point
 namespace ShowDateTime
 {
     enum class TimeType : uint8_t
@@ -34,7 +42,7 @@ namespace ShowDateTime
     // Arguments
     class AppArguments final : public Arguments
     {
-        const string ArgumentNameFormat = "format";
+        const string ArgumentNameFormat   = "format";
         const string ArgumentNameTimeType = "type";
 
         TimeTypeTextResolver TimeTypeTextConverter;
@@ -42,20 +50,22 @@ namespace ShowDateTime
     public:
         AppArguments()
         {
-            auto const defaultFormat = "yyyy-MM-dd HH:mm:ss";
+            auto const defaultFormat = "%Y-%m-%d %H:%M:%S.{fff}";
 
             AddOption(ValueType::STRING, "f", ArgumentNameFormat, defaultFormat, "The format to use to display the datetime value", false);
             AddOption(ValueType::ENUM, "t", ArgumentNameTimeType, TimeTypeTextConverter.GetText(TimeType::LOCAL), "The time value to use", false, 0, TimeTypeTextConverter.GetAllText());
 
-            AddNote("See the following for date formats:");
-            AddNote("  https://learn.microsoft.com/en-us/dotnet/standard/base-types/custom-date-and-time-format-strings");
+            AddNote("This uses 'strftime' internally, so see the following for date formats:");
             AddNote("  https://cplusplus.com/reference/ctime/strftime/");
             AddNote("As well as:");
-            AddNote("  {jjj} - Same as %j");
+            AddNote("  {fff} - Milliseconds - always 3 digits");
+            AddNote("  {f}   - Milliseconds - as few digits as possible");
             AddNote("  {qq}  - Month Quarter (left padded with 0 to 2 digits");
             AddNote("  {q}   - Month Quarter");
             AddNote("  {M}   - Month number (1 - 12)");
             AddNote("  {d}   - Day number (1 - 31)");
+            AddNote("  {H}   - 24 Hour number (0 - 23)");
+            AddNote("  {h}   - Hour number (0 - 12)");
         }
 
         string GetFormat()
@@ -68,47 +78,30 @@ namespace ShowDateTime
             return TimeTypeTextConverter.GetValue(GetArgumentValue(ArgumentNameTimeType));
         }
 
-        string GetConvertedFormatForBuiltIn()
+        string GetFormattedDateTime(const time_point<system_clock>& datetime)
         {
-            // As per : https://cplusplus.com/reference/ctime/strftime/
+            const auto datetime_t = system_clock::to_time_t(datetime);
+            tm datetime_tm;
+            auto err = localtime_s(&datetime_tm, &datetime_t);
 
-            auto text = GetFormat();
+            const auto quarter = GetQuarter(datetime_tm.tm_mon);
+            const auto milliseconds = GetMilliseconds(datetime);
 
-            text = StringUtils::ReplaceString(text, "yyyy", "%Y");
-            text = StringUtils::ReplaceString(text, "yy", "%y");
-            text = StringUtils::ReplaceString(text, "{jjj}", "%j");
-            text = StringUtils::ReplaceString(text, "dddd", "%A");
-            text = StringUtils::ReplaceString(text, "ddd", "%a");
-            text = StringUtils::ReplaceString(text, "dd", "%d");
-            text = StringUtils::ReplaceString(text, "MMMM", "%B");
-            text = StringUtils::ReplaceString(text, "MMM", "%b");
-            text = StringUtils::ReplaceString(text, "MM", "%m");
-            text = StringUtils::ReplaceString(text, "HH", "%H");
-            text = StringUtils::ReplaceString(text, "hh", "%I");
-            text = StringUtils::ReplaceString(text, "mm", "%M");
-            text = StringUtils::ReplaceString(text, "ss", "%S");
-            // TODO: Add other formats as necessary
-
-            return text;
-        }
-
-        string GetFormattedDateTime(const tm* datetime)
-        {
-            const auto quarter = GetQuarter(datetime->tm_mon);
-
-            const auto format = GetConvertedFormatForBuiltIn();
+            const auto format = GetFormat();
 
             char buffer[256];
-            const auto size = strftime(buffer, sizeof(buffer), format.c_str(), datetime);
+            const auto size = strftime(buffer, sizeof(buffer), format.c_str(), &datetime_tm);
 
             auto text = string(buffer, size);
 
+            text = StringUtils::ReplaceString(text, "{fff}", StringUtils::LPad(std::to_string(milliseconds), 3, '0'));
+            text = StringUtils::ReplaceString(text, "{f}", std::to_string(milliseconds));
             text = StringUtils::ReplaceString(text, "{qq}", StringUtils::LPad(std::to_string(quarter), 2, '0'));
             text = StringUtils::ReplaceString(text, "{q}", std::to_string(quarter));
-            text = StringUtils::ReplaceString(text, "{M}", std::to_string(datetime->tm_mon));
-            text = StringUtils::ReplaceString(text, "{d}", std::to_string(datetime->tm_mday));
-            text = StringUtils::ReplaceString(text, "{H}", std::to_string(datetime->tm_hour));
-            text = StringUtils::ReplaceString(text, "{h}", std::to_string(datetime->tm_hour > 11 ? datetime->tm_hour - 12 : datetime->tm_hour));
+            text = StringUtils::ReplaceString(text, "{M}", std::to_string(datetime_tm.tm_mon + 1));
+            text = StringUtils::ReplaceString(text, "{d}", std::to_string(datetime_tm.tm_mday));
+            text = StringUtils::ReplaceString(text, "{H}", std::to_string(datetime_tm.tm_hour));
+            text = StringUtils::ReplaceString(text, "{h}", std::to_string(datetime_tm.tm_hour > 11 ? datetime_tm.tm_hour - 12 : datetime_tm.tm_hour));
 
             return text;
         }
@@ -121,6 +114,16 @@ namespace ShowDateTime
             }
 
             return (month / 3) + 1;
+        }
+
+        static int GetMilliseconds(const time_point<system_clock> datetime)
+        {
+            const auto seconds = std::chrono::time_point_cast<std::chrono::seconds>(datetime);
+            const auto fraction = datetime - seconds;
+
+            const auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(fraction);
+
+            return static_cast<int>(milliseconds.count());
         }
     };
 }
