b44a19261024e9d97ec47df5c72756f196493e04
Martin Smith <martinsmith1968@gmail.com>
2026-01-12 00:41:23 +0000

    ShowDateTime includes repointed

--- apps/ShowDateTime/AppArguments.h
+++ apps/ShowDateTime/AppArguments.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #include "stdafx.h"
-#include "../DNX.App/Arguments.h"
-#include "../DNX.Utils/StringUtils.h"
+#include "../../libs/DNX.App/Arguments.h"
+#include "../../libs/DNX.Utils/StringUtils.h"
 #include <chrono>
 #include <string>
 
--- apps/ShowDateTime/AppInfo.h
+++ apps/ShowDateTime/AppInfo.h
@@ -5,4 +5,4 @@
 #define VER_PRODUCTNAME_STR         "ShowDateTime"
 #define VER_FILE_DESCRIPTION_STR    "Show current date and time formatted"
 
-#include "../Common/AppInfo.h"
+#include "../../Common/AppInfo.h"
